e2e0337a8d8003aaf4fb292bf2c0325ee0643240
Patrick Gutting <patrick-gutting@t-online.de>
2018-12-01 13:59:11 +0100

    Commit
    Signed-off-by: Patrick Gutting <patrick-gutting@t-online.de>

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hallo Johann</Text>
+        <Text>Hallo Patrick123</Text>
       </View>
     );
   }
