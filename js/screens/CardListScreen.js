270d5ce4e6e3b8afb31f4a4f1b11f38798b7493d
Breitenbücher <breitenbue@de.cenit-group.com>
2018-12-14 17:01:35 +0100

    jb

--- js/screens/CardListScreen.js
+++ js/screens/CardListScreen.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
-import { Alert, ActivityIndicator, Dimensions, View, FlatList, TouchableOpacity, ScrollView, StyleSheet, Text } from 'react-native';
+import { Alert, ActivityIndicator, Dimensions, View, FlatList, TouchableOpacity, ScrollView, StyleSheet } from 'react-native';
 // import database
 import Firebase from '../Firebase';
 
 export default class CardListScreen extends Component {
 
-    // state = { userData: {}, showCreateSubjectScreen: false, subjects: [], isLoading: true };
+    state = { cards: [], isLoading: true };
 
     // set cardList title at runtime
     static navigationOptions = ({ navigation }) => {
         const cardList = navigation.getParam('cardList');
+
         return {
             title: cardList.name
         };
     };
 
-    /* called after view is called */
-    componentDidMount() {
-        // init firebase object        
-        Firebase.init();
+    _retrieveCardList = async () => {
+        //Liste der Karteikarten aus der Firebase laden
     }
 
     render() {
-        debugger;
-        return (
-            <View style={styles.container}>
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator />
+                </View>);
+        }
 
+        return (
+            <ScrollView style={styles.container} >
+
+
+                <FlatList>
+
+                </FlatList>
 
                 {/* <FlatList
-                    data={this.state.subjects}
+                    data={this.state.cards}
                     keyExtractor={item => item.name}
                     ItemSeparatorComponent={() => <View style={styles.listSeparator} />}
                     ListEmptyComponent={() => (<Text style={styles.listEmpty}>No Data</Text>)}
@@ -36,24 +45,44 @@ export default class CardListScreen extends Component {
                     onRefresh={this._refresh}
                     renderItem={({ item }) => (
                         // render SubjectListItems
-                        <SubjectListItem subject={item} onPress={() => this.props.navigation.navigate('SubjectDetailScreen', {
-                            subject: item
-                        })}></SubjectListItem>
+                        <CardListItem subject={item} onPress={() => this.props.navigation.navigate('CardDetailScreen', {
+                            cards: item
+                        })}></CardListItem>
                     )}
                 /> */}
 
-
-                <Text>Dann Hau mal rein :D</Text>
-            </View>
+            </ScrollView>
         );
     }
-}
 
+
+    // Called after the view is loaded
+    componentDidMount() {
+        // init firebase
+        Firebase.init();
+        // get the cards of the current deck
+        this._retrieveCards();
+    }
+    // Get cards from database
+    _retrieveCards = async () => {
+        let cards = this.props.navigation.getParam('cards');
+    }
+}
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
         backgroundColor: 'red',
+    },
+    listSeparator: {
+        height: StyleSheet.hairlineWidth,
+        backgroundColor: 'lightsalmon',
+        marginVertical: 5
+    },
+    listEmpty: {
+        paddingTop: 100,
+        fontSize: 30,
+        textAlign: 'center'
     }
 });
 
